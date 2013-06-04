e51d4c799eb861758339e76ee807e84d44eef8d3
Mihai-Radu-Niculae Popescu <mp4511@imperial.ac.uk>
2013-06-04 16:18:02 +0100

    Created the servlet for basic authentication. As of now it adds a cookie with a username for the current user.

---
+++ tomcat_main/src/AuthServlet.java
@@ -0,0 +1,51 @@
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Cookie;
+import java.sql.ResultSet;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.sql.SQLException;
+
+
+public class AuthServlet extends HttpServlet {
+
+  public static final String QUERY_SEGMENT 
+      = "SELECT password FROM Users WHERE username = "; 
+  @Override
+  public void doPost(HttpServletRequest request,
+                     HttpServletResponse response) {
+    String user = request.getParameter("username"),
+           pass = request.getParameter("password");
+    try {
+      PsqlQuery authQuery = new PsqlQuery();
+      ResultSet rs = authQuery.executeQuery(QUERY_SEGMENT + "'" + user + "';");
+      if(!rs.next() || !rs.getString("password").equals(pass)) {
+        sendInvalidLogin(response);
+        return;
+      }
+      // The response is right so we redirect it to the homepage with an added cookie
+      authenticateUser(response, user);
+    } catch (SQLException e) {
+      sendInternalError(response, e.getMessage());
+    }
+  }
+  private void sendInvalidLogin(HttpServletResponse res) {
+    try {
+      res.sendRedirect("/invalidLogin.html");
+    } catch(IOException ioe) {}
+  }
+
+  private void sendInternalError(HttpServletResponse res, String msg) {
+    try {
+      res.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, msg);
+    } catch(IOException ioe) {}
+  }
+
+  private void authenticateUser(HttpServletResponse res, String user) {
+    try {
+      res.addCookie(new Cookie("username",user));
+      res.sendRedirect("/hello.html");
+    } catch (IOException ioe) {}
+  }
+}
---
+++ tomcat_main/src/PsqlQuery.java
@@ -0,0 +1,34 @@
+import org.postgresql.Driver;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.SQLException;
+
+public class PsqlQuery {
+
+  private final static String CONN_STR 
+      = "jdbc:postgresql://db.doc.ic.ac.uk/g1227107_u",
+                              DB_USER = "g1227107_u",
+                              DB_PASS = "krCgD6QJtX";
+
+  private Connection db_conn;
+  private Statement stmt;
+
+  public PsqlQuery() throws SQLException{
+    db_conn = DriverManager.getConnection(CONN_STR, DB_USER, DB_PASS);
+    stmt = db_conn.createStatement();
+
+  }
+
+  public ResultSet executeQuery(String req) throws SQLException{
+    // non-atomic query
+    return stmt.executeQuery(req);
+  }
+
+  public void finish() throws SQLException{
+    db_conn.close();
+  }
+}
+
+  
