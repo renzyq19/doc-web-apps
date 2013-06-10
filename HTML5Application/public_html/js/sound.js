7610a4a1e631020a52871fc7633d30eda410de19
Conrad Godfrey <conradjgodfrey@tiscali.co.uk>
2013-06-10 15:36:49 +0100

    Added more sounds

---
+++ HTML5Application/public_html/js/sound.js
@@ -0,0 +1,12 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+ createjs.Sound.registerSound("audio/shot.ogg","shot");
+ createjs.Sound.registerSound("audio/impact.ogg","impact");
+ createjs.Sound.registerSound("audio/destruction.ogg","destruction");
+ createjs.Sound.registerSound("audio/victory.ogg", "victory");
+
+
